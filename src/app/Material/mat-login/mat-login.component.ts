b09c89d7962dae28ff1df2eb8f6038e0b1a48c63
Siddhartha Pathak <siddhartha.pathak@krantitechservices.com>
2022-07-26 02:33:33 +0530

    MAterial Login

--- src/app/Material/mat-login/mat-login.component.ts
+++ src/app/Material/mat-login/mat-login.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroupDirective, NgForm, Validators, FormGroup} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+import {  Router } from '@angular/router';
+import { AuthService } from 'src/app/auth.service';
 
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 @Component({
   selector: 'app-mat-login',
   templateUrl: './mat-login.component.html',
   styleUrls: ['./mat-login.component.css']
 })
 export class MatLoginComponent implements OnInit {
+  loginForm= new FormGroup({
+    email : new FormControl(''),
+    password : new FormControl(''),
+  })
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required]);
 
-  constructor() { }
-
+  matcher = new MyErrorStateMatcher();
+  constructor(private auth:AuthService, private route: Router) { }
   ngOnInit(): void {
+  }
+  submit(){
+    
+    console.log(this.loginForm)
+    this.auth.loginUser(this.loginForm.value).subscribe(
+      res => {
+        localStorage.setItem('token', res.token)
+        this.route.navigate(['/material/material-user'])
+      },
+      err => {
+        console.log(err)
+      }
+    )
   }
 
 }
